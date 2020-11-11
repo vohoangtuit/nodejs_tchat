24a39e6887373d9d48eb249d0b3862b15044c67f
vohoangtuit <vohoangtu2401@gmail.com>
2020-11-11 11:41:33 +0700

    create project and connect to firebase

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+const expresss = require('express');
+const app = expresss();
+app.get('/timestamp',(request,response)=>{
+    response.send(`${Date.now()}`);
+});
+app.get('/timestamp-cached',(request,response)=>{
+    response,set('Cache-Control','public,max-age=300,s-maxage=600');
+    response.send(`${Date.now()}`);
+});
+
+
+exports.app = functions.https.onRequest(app);
+// todo url on web : https://tchat-526d5.firebaseapp.com/timestamp
